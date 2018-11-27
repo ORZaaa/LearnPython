6af7f98d4137a059379c3b8cc10bcdba4908bb29
ORZaaa <chenchendemail@gmail.com>
2018-11-28 01:14:55 +0800

    add type of data

--- _Project/Course_1.py
+++ _Project/Course_1.py
@@ -1,10 +1,37 @@
+import math
+
 # string
 print('hello,world!')
 
 # lib & int/float
-import math
+
 
 x = 100 + 200
 y = math.pi * math.e
 
 print(x,y)
+
+# type of data
+
+# int
+print(1, 3, 100, -800)
+
+# float
+print(3.14158, math.pi, math.e, 12.3e8, 1.2e-5)
+
+# string
+print('''
+'hello world'
+ "hello"
+ "I'm OK"
+ 'I\'m \"OK\"!'
+ r'\\\t\\'
+''','hello \nworld', "hello", "I'm OK", 'I\'m \"OK\"!', r'\\\t\\')
+
+# bool
+b1 = True and True
+b2 = True or False
+b3 = not False
+b4 = 5 > 3 and 3 > 1
+
+print(b1, b2, b3, b4)
